270fa5c9c6398eb11f1a00538ddd93cf464ecdf4
Rohith Suddala <76805560+rohith4463@users.noreply.github.com>
2025-09-09 01:16:28 -0500

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,9 @@
+maths = int(input("marks"))
+science = int(input("marks"))
+social = int(input("marks"))
+tegulu = int(input("marks"))
+hindi = int(input("marks"))
+
+sum = (maths+science+social+tegulu+hindi)/500 * 100
+
+print(f"percentage of five subjects {sum}")
