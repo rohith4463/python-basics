25f5bf33526b090a7df1ff56c3fcbb073d79fae7
Rohith Suddala <76805560+rohith4463@users.noreply.github.com>
2025-09-14 23:30:27 -0500

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,29 @@
+"""list_length5  = int(input(" enter a length = "))
+#print the all number in list
+result= [ ]
+
+for i in range(0,list_length):
+    num = int(input("enter a number = "))
+    result.append(num)
+print(result)"""
+
+"""my_list = [23,27,45,87,44]
+old=23
+new=98
+
+# old number change to new number
+for i in range(0,len(my_list)):
+    if my_list[i]==old:
+        my_list[i]=new
+        
+print(my_list)
+"""
+
+a=[24,33,64,98,55]
+b=[]
+
+for i in a:
+    if i % 2 != 0:
+        b.append(i)
+print(b)
+
