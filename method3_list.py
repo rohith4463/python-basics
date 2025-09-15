5e30270830908188f801e44017a4c6bbc795fb9b
Rohith Suddala <76805560+rohith4463@users.noreply.github.com>
2025-09-15 00:55:52 -0500

    Create method3_list.py

---
+++ method3_list.py
@@ -0,0 +1,10 @@
+my_list=[25,43,67,12,10,35]
+my_list2=[12,11,34,57,88,97,21,23,44,46]
+result=[]
+for i in my_list:
+
+    result.append(i)
+for i in my_list2:
+        
+        result.append(i)
+print(f"merged {result}")
