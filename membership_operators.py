611e676891ebb61f7c17e88a7409980ca4e04272
Rohith Suddala <76805560+rohith4463@users.noreply.github.com>
2025-09-15 01:07:44 -0500

    Create membership_operators.py

---
+++ membership_operators.py
@@ -0,0 +1,9 @@
+a = [34,45,"rohith","roll no",65,98,00]
+# membership operators are IN and NOT In 
+
+# ask a number for the user if number is present in the list print yes or print no
+num = int(input("enter a number = "))
+if num in a:
+    print("yes")
+else:
+    print("No")
