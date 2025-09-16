c205c5ab515ebd0074e3d1bf0c4c92c6f74556ac
Rohith Suddala <76805560+rohith4463@users.noreply.github.com>
2025-09-16 02:08:54 -0500

    Create into_to_strings.py

---
+++ into_to_strings.py
@@ -0,0 +1,34 @@
+# strings are Iterable
+# Immutable
+# in strings we cannt changed any value
+
+"""my_string = "code and decoding"
+
+# print the string in reverse
+
+for i in range(len(my_string)-1,-1,-1):
+    print(my_string[i])"""
+
+# different operation on strings
+
+"""a="rohith"
+b="developer"""
+#concadinate
+#print(a+b)
+
+# we cannt multipy with string to string ; we can multipy with string with number
+
+#print(a==b)
+
+#ASCII CODE will compare with ascii code because it will covert
+
+# to dec to char and char to dec
+#Every char has a dec value in ascii code
+
+#print(a>b)
+
+#print(chr(99))
+
+x= "e"
+print(ord(x))
+
